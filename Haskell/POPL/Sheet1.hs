eed526931d902c02d1872a27d709755743df5264
Liora <lioraweisz@gmail.com>
2020-10-31 16:56:09 +0100

    ML practical 1

--- Haskell/POPL/Sheet1.hs
+++ Haskell/POPL/Sheet1.hs
@@ -5,13 +5,14 @@ import Prelude hiding (&&)-}
 -- index :: Eq a => a -> [a] -> Int
 -- index x [] = -1
 -- index x (y:ys) = if x==y then 0 else let r=index x ys in if r==(-1) then (-1) else 1+r
+import Memory
 
 
 index :: Eq a => a -> [a] -> Int
 index x [] = -1
-index x (y:ys) = if x==y then 0 else let r=index x ys in if r==(-1) then (-1) else 1+r
+index x (y:ys) = if x==y then 0 else let r=Main.index x ys in if r==(-1) then (-1) else 1+r
 
-index' :: Eq a => a -> [a] -> Int
+--index' :: Eq a => a -> [a] -> Int
 index' x ys = index'' x 0 ys
 index'' i x [] = -1
 index'' i  x (y:ys) = if x==y then i else index'' x (i+1) ys
@@ -23,5 +24,44 @@ retrieve :: [Int] -> Int
 retrieve [] = -1
 retrieve (x:xs) = x
 
-eval (List es) env = map (ev es)
-where ev e = eval e env
+--eval (List es) env = map (ev es)
+--where ev e = eval e env
+
+
+data Value =
+    IntVal Integer			-- Integers
+  | BoolVal Bool			-- Booleans
+  | Nil 				-- Empty list
+  | Cons Value Value			-- Non-empty lists
+  | Addr Location
+  | Function ([Value] -> M Value)
+
+infixl 1 $>
+
+type Mem = Memory Value
+
+type M a = Mem -> (String, a , Mem)
+
+result :: a -> M a
+result x mem = ("", x , mem)
+
+($>) :: M a -> (a -> M b) -> M b
+($>) xm f mem = let (s, x , mem') = xm mem in let (t, y, mem'') = f x mem' in (s++t, y, mem'')
+
+get :: Location -> M Value
+get a mem = ("", contents mem a, mem)
+
+put :: Location -> Value -> M()
+put a v mem = ("", (), update mem a v)
+
+new :: M Location
+new mem = let (o, m) = fresh mem in ("", o, m)
+
+output :: String -> M()
+output s mem = (s, (), mem)
+
+eval (Apply f es) env =
+  revevalargs es env $> (\ args ->
+    eval f env $> (\ fv ->
+      apply fv args))
+  where revevalargs es env = reverse (evalargs (reverse es) env)
